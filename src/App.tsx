cab3e654a03b40a7416e5700f026e665f6f89703
arslanBekboltov <arslanbekbolotov58@gmail.com>
2023-05-26 18:30:40 +0600

    Добавил функции стейты и передал их в компонент

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Block from "./components/Box/Block";
 
+interface IBLock{
+    hasItem: boolean;
+    clicked: boolean;
+}
+
 function App() {
+    let tries:number = 0;
+    const createItems=()=>{
+        let box:IBLock[] =  [];
+        const randomIndex = Math.floor(Math.random() * (35 + 1));
+
+        for(let i=1;i <= 36;i++){
+            box.push({hasItem: false, clicked: false});
+        }
+
+        box[randomIndex].hasItem = true;
+        return box;
+    }
+
+    const [items, setItems] = useState(createItems());
+    const [count,setCount]=useState(0);
+
+    const ShowBlockContent = (index:number,hasItem:boolean) =>{
+        const itemsCopy = [...items];
+        const itemCopy = {...itemsCopy[index]};
+        itemCopy.clicked = true;
+        itemsCopy[index]=itemCopy;
+        setItems(itemsCopy);
+        if(!hasItem){
+            setCount(count + 1);
+        }else{
+            alert('Вы нашли клад');
+        }
+    }
+
+
   return (
     <div className="App">
         <div className="box">
-            <Block hasItem={true} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
-            <Block hasItem={false} clicked={true}/>
+            {items.map((item,index)=>{
+                return(
+                    <Block
+                        hasItem={item.hasItem}
+                        clicked={item.clicked}
+                        key={index}
+                        onClickBlock={()=>ShowBlockContent(index,item.hasItem)}
+                    />
+                )
+            })}
+            <p>Количество попыток:{count}</p>
         </div>
 
     </div>
--- src/components/Box/Block.tsx
+++ src/components/Box/Block.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
+import "./Box.css"
 interface IProps{
     hasItem: boolean;
     clicked: boolean;
+    onClickBlock:React.MouseEventHandler<HTMLDivElement>;
 }
-const Block:React.FC<IProps> = ({hasItem,clicked}) => {
+
+const Block:React.FC<IProps> = ({hasItem,clicked,onClickBlock}) => {
     if(hasItem && clicked){
         return(
-            <div className='block transparent'>
-                <img src="https://cdn0.iconfinder.com/data/icons/Jewelry/256/Treasure.png" alt="treasure"/>
+            <div onClick={onClickBlock} className='block transparent'>
+                <img className="boxImg" src="https://cdn0.iconfinder.com/data/icons/Jewelry/256/Treasure.png" alt="treasure"/>
             </div>
         )
+
+    } else if(!hasItem && clicked){
+        return <div onClick={onClickBlock} className='block transparent'></div>
+    }else{
+        return <div onClick={onClickBlock} className='block'></div>
     }
 
-    if(!hasItem && clicked){
-        return(
-            <div className='block transparent'></div>
-            )
-    }
-
-    
-    return (
-        <div className='block'></div>
-    );
 };
 
 export default Block;
